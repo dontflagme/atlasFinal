ae9be31cef1eafe153049eaac77a2cc7815c395a
Brian Saylor <bsaylor3@mail.greenriver.edu>
2017-09-11 01:36:33 -0500

    added index and vendor to get rid of errors

--- index.php
+++ index.php
@@ -22,33 +22,15 @@ require("../../../atlas_config.php");
 //Create an instance of the Base class
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
-<<<<<<< HEAD
 
-
-//calling the DB classes
-//$eventsDB = new EventDB();
-//$memberDB = new MemberDB();
-//$joinedDB = new JoinedDB();
-
-=======
-  
     //$eventDB = new EventsDB();
     //$memberDB = new MemberDB();
     //$joinedDB = new JoinedDB();
->>>>>>> fbe9d1fddc2a516aa924b444538ca5f92e57c9b6
 
                   //Define a default route
                     $f3->route('GET /', function($f3) {
                         $events =  $GLOBALS['$eventDB']->allEvents();
                         $f3->set('events', $events);
-                      echo Template::instance()->render('pages/Frontend/explore.html');
-                      
-                     });
-                    
-                    
-                  //Define a default route
-                    $f3->route('GET /logout', function($f3) {
-                      
                       echo Template::instance()->render('pages/Frontend/explore.html');
                      });
                     
@@ -74,25 +56,6 @@ $f3->set('DEBUG', 3);
                       echo Template::instance()->render('pages/backend/addIdea.html');
                      });
                     
-                    //takes user register infor
-                    $f3->route('POST /registerDB', function($f3) {
-                      $fname = $_POST['firstname'];
-                      $lname = $_POST['lastname'];
-                      $email = $_POST['email'];
-                      $password = $_POST['password'];
-                      
-                      if(preg_match('~[0-9]~', $fname) || preg_match('~[0-9]~', $lname) || preg_match('/[^a-zA-Z]+/', $fname) || preg_match('/[^a-zA-Z]+/', $lname)){
-                             echo "First name or last name can not contain numbers or special characters, silly!";
-                      }
-                      
-                      else{
-                        echo "Member created.";
-                      }
-                      
-
-                      
-                      
-                     });
            
 $f3->run();        
 ?>
